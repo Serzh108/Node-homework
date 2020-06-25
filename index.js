5c40ec695b33f2b2ec5c8968e20ffa1e3ba6e550
Serzh108 <serzh@gabris.kiev.ua>
2020-06-25 15:02:27 +0300

    add a litle

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+// const morgan = require('morgan');
 
 const app = express();
 app.use(cors());
 const PORT = 3000;
 
-app.get('/', function (request, respons, next) {
-  respons.send('Hi from my server');
-});
+app.get('/api/contacts', listContacts
+// function (request, respons, next) {
+//   respons.send('Hi from my server and /api/contacts');
+// }
+);
+
+function listContacts(req, res, next) {
+  return res.status(200).json('<h1>res=true</h1>');
+}
 
 app.listen(PORT, function () {
-  console.log('Listen onport: ', PORT)
+  console.log('Listen onport: ', PORT);
 });
