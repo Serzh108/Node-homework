0f32749be86fa01811cfc6a5c01ecd100ce8ddb5
Serzh108 <serzh@gabris.kiev.ua>
2020-06-27 17:01:00 +0300

    Node.js Homework-02 completed

--- contact.js
+++ contact.js
@@ -4,74 +4,84 @@ const path = require('path');
 
 const contactsPath = path.join(__dirname, 'db/contacts.json');
 module.exports.contactsPath = contactsPath;
-// console.log('contactsPath = ', contactsPath);
-// ================================================
-// const dataFromJson = fs.readFileSync(`${contactsPath}`, "utf-8");
-// console.log('dataFromJson : ', dataFromJson);
 // ================================================
 module.exports.listContacts = async function (pathToFile) {
   try {
-    await fsPromises
-      .readFile(pathToFile, 'utf-8')
-      .then(res => console.table(JSON.parse(res)));
-    // .then(res => console.log('Contacts list: ',  JSON.parse(res)));
+    const result = await fsPromises.readFile(pathToFile, 'utf-8');
+    return result;
   } catch (err) {
     throw err;
   }
 };
-
-// listContacts(contactsPath);
 // ================================================
 module.exports.getContactById = async function (pathToFile, contactId) {
   try {
-    await fsPromises.readFile(pathToFile, 'utf-8').then(res => {
-      const result = JSON.parse(res);
-      const contact = result.find(item => item.id === contactId);
-      console.table(contact);
-      // console.log('contact = ', contact);
-    });
+    const ContactById = await fsPromises
+      .readFile(pathToFile, 'utf-8')
+      .then(res => {
+        const result = JSON.parse(res);
+        const contact = result.find(item => item.id === contactId);
+        return contact;
+      });
+    return ContactById;
   } catch (err) {
     throw err;
   }
 };
-
-// getContactById(7);
 // ================================================
 module.exports.removeContact = async function (pathToFile, contactId) {
   try {
-    await fsPromises.readFile(pathToFile, 'utf-8').then(res => {
-      const result = JSON.parse(res);
-      const newContact = result.filter(item => item.id !== contactId);
-      // console.log('newContact = ', newContact);
-      fsPromises.writeFile(pathToFile, JSON.stringify(newContact));
-    });
+    const deleteContact = await fsPromises
+      .readFile(pathToFile, 'utf-8')
+      .then(res => {
+        const result = JSON.parse(res);
+        const newContact = result.filter(item => item.id !== contactId);
+        fsPromises.writeFile(pathToFile, JSON.stringify(newContact));
+        return result.length === newContact.length;
+      });
+    return deleteContact;
   } catch (err) {
     throw err;
   }
 };
-
-// removeContact(3);
 // ================================================
-module.exports.addContact = async function (pathToFile, name, email, phone) {
+module.exports.addContact = async function (pathToFile, newUser) {
   try {
-    await fsPromises.readFile(pathToFile, 'utf-8').then(res => {
-      const result = JSON.parse(res);
-      const lastContactId = result[result.length - 1].id;
-      // console.log('lastContactId = ', lastContactId);
-      const newContact = {
-        id: lastContactId + 1,
-        name,
-        email,
-        phone,
-      };
-      result.push(newContact);
-      // console.log('result = ', result);
-      fsPromises.writeFile(pathToFile, JSON.stringify(result));
-    });
+    const newContact = await fsPromises
+      .readFile(pathToFile, 'utf-8')
+      .then(res => {
+        const result = JSON.parse(res);
+        result.push(newUser);
+        fsPromises.writeFile(pathToFile, JSON.stringify(result));
+        return newUser;
+      });
+    return newContact;
   } catch (err) {
     throw err;
   }
 };
-
-// addContact('Vasya Pupkin', 'Vasya.Pupkin@vestibul.com', '(044) 098-7654');
+// { "name":  "Bob Pupkin", "email": "Vasya.Pupkin@vestibul.com", "phone": "(044) 098-7654" }
+// ================================================
+module.exports.updateContact = async function (
+  pathToFile,
+  contactId,
+  updetedData,
+) {
+  try {
+    const updetedContact = await fsPromises
+      .readFile(pathToFile, 'utf-8')
+      .then(res => {
+        const result = JSON.parse(res);
+        const updetedContacts = result.map(item =>
+          item.id === contactId ? { ...item, ...updetedData } : item,
+        );
+        fsPromises.writeFile(pathToFile, JSON.stringify(updetedContacts));
+        const contact = updetedContacts.find(item => item.id === contactId);
+        return contact;
+      });
+    return updetedContact;
+  } catch (err) {
+    throw err;
+  }
+};
 // ================================================
