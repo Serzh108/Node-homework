3f74a4edb85542f9d322bb6e0e134ebda02ecb6e
Serzh108 <serzh@gabris.kiev.ua>
2020-07-24 11:53:24 +0300

    Inttial state Homework-06 Node.js

---
+++ src/test/auth-middleware.spec.js
@@ -0,0 +1,9 @@
+describe('Authmiddleware test suite', () => {
+  before(() => {});
+  beforeEach(() => {});
+  afterEach(() => {});
+  after(() => {});
+  it('should test', () => {
+    console.log('first test');
+  });
+});
